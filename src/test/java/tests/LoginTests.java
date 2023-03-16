810d1cbd08b1331979257d29df7ffea9fad55c18
MarinaSokolnikova <sokolnikovam92@gmail.com>
2023-03-16 21:03:17 +0200

    loginSuccess

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,11 +1,35 @@
 package tests;
 
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase{
 
+    @BeforeMethod
+    public void preCondition(){
+        if (app.getHelperUser().isLogged())
+        {
+            app.getHelperUser().logout();
+        }
+    }
     @Test
-    public void test(){
-        
+    public void loginSuccess(){
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("ssa@gmail.com", "Ssa12345$");
+        app.getHelperUser().submitLogin();
+
+        //Assert
+        Assert.assertTrue(app.getHelperUser().isLogged());
+    }
+
+    @Test
+    public void loginSuccessModel(){
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("ssa@gmail.com", "Ssa12345$");
+        app.getHelperUser().submitLogin();
+
+        //Assert
+        Assert.assertTrue(app.getHelperUser().isLogged());
     }
 }
