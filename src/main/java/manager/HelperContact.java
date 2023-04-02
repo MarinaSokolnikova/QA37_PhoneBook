0da1117460e385d6cf378fd322038f42a060e050
MarinaSokolnikova <sokolnikovam92@gmail.com>
2023-04-02 12:02:37 +0300

    homework

---
+++ src/main/java/manager/HelperContact.java
@@ -0,0 +1,28 @@
+package manager;
+
+import models.Contact;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class HelperContact extends HelperBase{
+    public HelperContact(WebDriver wd) {
+        super(wd);
+    }
+
+    public void openAddForm() {
+        click(By.xpath("//a[@href='/add']"));
+    }
+
+    public void fillAddForm(Contact contact) {
+        type(By.xpath("//input[@placeholder='Name']"), contact.getName());
+        type(By.xpath("//input[@placeholder='Last Name']"), contact.getLastName());
+        type(By.xpath("//input[@placeholder='Phone']"), contact.getPhone());
+        type(By.xpath("//input[@placeholder='email']"), contact.getEmail());
+        type(By.xpath("//input[@placeholder='Address']"), contact.getAddress());
+        type(By.xpath("//input[@placeholder='description']"), contact.getDescription());
+    }
+
+    public void submitSave() {
+        click(By.xpath("//div[@class='add_form__2rsm2']/button"));
+    }
+}
---
+++ src/test/java/tests/AddNewContactTests.java
@@ -0,0 +1,60 @@
+package tests;
+
+import models.Contact;
+import models.User;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.Random;
+
+public class AddNewContactTests extends TestBase{
+
+    @BeforeClass
+    public void preCondition(){
+        User user = new User().setEmail("ssa@gmail.com").setPassword("Ssa12345$");
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm(user);
+        app.getHelperUser().submitLogin();
+    }
+
+    @Test
+    public void addNewContactSuccessAll() {
+        Random random = new Random();
+        int randomPhone = random.nextInt(1000);
+        int randomEmail = random.nextInt(1000);
+        Contact contact = Contact.builder()
+                .name("Bob")
+                .lastName("Smit")
+                .phone("6471746178"+ randomPhone)
+                .email("bob" + randomEmail + "@gmail.com")
+                .address("Tel Aviv")
+                .description("my friend")
+                .build();
+        app.getHelperContact().openAddForm();
+        app.getHelperContact().fillAddForm(contact);
+        app.getHelperContact().submitSave();
+
+        Assert.assertTrue(app.getHelperUser().isElementPresent(By.xpath("//div[@class='contact-item_card__2SOIM']/h3[text()='"+ contact.getPhone()+ "']")));
+    }
+
+    @Test
+    public void addNewContactSuccess() {
+        Random random = new Random();
+        int randomPhone = random.nextInt(1000);
+        int randomEmail = random.nextInt(1000);
+        Contact contact = Contact.builder()
+                .name("Cris")
+                .lastName("Braun")
+                .phone("917201736102"+ randomPhone)
+                .email("cris" + randomEmail + "@gmail.com")
+                .address("Rehovot")
+                .build();
+        app.getHelperContact().openAddForm();
+        app.getHelperContact().fillAddForm(contact);
+        app.getHelperContact().submitSave();
+
+        Assert.assertTrue(app.getHelperUser().isElementPresent(By.xpath("//div[@class='contact-item_card__2SOIM']/h3[text()='"+ contact.getPhone()+ "']")));
+    }
+}
